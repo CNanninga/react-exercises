4be84db00df2bfdab90a9c7c6dbef8f8131903f3
Chris Nanninga <tarsusc81@gmail.com>
2022-04-05 14:24:08 -0500

    Replace lists example with function component version that already existed

--- src/07b_lists/index.js
+++ src/07b_lists/index.js
@@ -6,62 +6,67 @@ function ListItem(props) {
     );
 }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            direction: null,
-            users: [
-                {
-                    id: '12345',
-                    username: 'Carl',
-                },
-                {
-                    id: '34567',
-                    username: 'Kathy',
-                },
-                {
-                    id: '45678',
-                    username: 'Frank',
-                }
-            ]
-        };
-    }
+function App (props) {
+    const {getUsers, sort, timesSorted} = useApp();
 
-    sort() {
-        let direction = this.state.direction;
-        let users = this.state.users;
-        direction = (direction === null || direction == 'desc') ? 'asc' : 'desc';
-        users = users.sort((userA, userB) => {
-            if (direction == 'asc') {
+    return (
+        <div>
+            <h1>Lists</h1>
+            <p><button onClick={sort}>Sort</button><br />
+            Times sorted: {timesSorted}</p>
+            <ul>
+                {getUsers().map((user) => (
+                    <ListItem key={user.id} id={user.id} username={user.username} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
+
+
+
+function useApp () {
+    const [direction, setDirection] = React.useState(null);
+    const [users, setUsers] = React.useState([
+        {
+            id: '12345',
+            username: 'Carl',
+        },
+        {
+            id: '34567',
+            username: 'Kathy',
+        },
+        {
+            id: '45678',
+            username: 'Frank',
+        }
+    ]);
+    const [timesSorted, setTimesSorted] = React.useState(0);
+
+    const getUsers = React.useCallback(function() {
+        return users;
+    }, [users]);
+
+    const sort = React.useCallback(function() {
+        let newDirection = (direction === null || direction == 'desc') ? 'asc' : 'desc';
+        let newUsers = users.sort((userA, userB) => {
+            if (newDirection == 'asc') {
                 return (userB.username > userA.username) ? -1 : 1;
-            } else if (direction == 'desc') {
+            } else if (newDirection == 'desc') {
                 return (userA.username > userB.username) ? -1 : 1;
             }
             return 0;
         });
+        let newTimesSorted = timesSorted + 1;
 
-        this.setState({
-            direction: direction,
-            users: users
-        });
-    }
+        setDirection(newDirection);
+        setUsers(newUsers);
+        setTimesSorted(newTimesSorted);
+    }, [direction, setDirection, users, setUsers, timesSorted, setTimesSorted]);
 
-    render() {
-        const users = this.state.users;
-
-        return (
-            <div>
-                <h1>Lists</h1>
-                <p><button onClick={this.sort.bind(this)}>Sort</button></p>
-                <ul>
-                    {users.map((user) => (
-                        <ListItem key={user.id} id={user.id} username={user.username} />
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+    return {getUsers, sort, timesSorted};
 }
 
 export default App;
