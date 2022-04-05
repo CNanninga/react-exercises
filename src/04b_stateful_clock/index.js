57b99ad7016e3d456299d42bd9d7976ee92af7f1
Chris Nanninga <tarsusc81@gmail.com>
2022-04-05 14:00:38 -0500

    Convert clock to function component

--- src/04b_stateful_clock/index.js
+++ src/04b_stateful_clock/index.js
@@ -1,38 +1,31 @@
 import React from 'react';
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date()
-        };
-    }
+function Clock(props) {
+    const [date, setDate] = React.useState(new Date());
 
-    componentDidMount() {
-        this.timerId = setInterval(
+    let timerId = null;
+
+    React.useEffect(() => {
+        timerId = setInterval(
             () => this.tick(),
             1000
         );
+
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []);
+
+    function tick() {
+        setDate(new Date());
     }
 
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {date.toLocaleTimeString()}.</h2>
+        </div>
+    );
 }
 
 export default Clock;
